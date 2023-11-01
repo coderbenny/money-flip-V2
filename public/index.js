af02fe027e21343186d0c3501e8482a1fa8a894e
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-11-01 15:28:02 +0300

    Updated the initialize function

--- public/index.js
+++ public/index.js
@@ -1,20 +1,36 @@
-const ApiKey = '92b9cff50c9745fc9fc4212793f6ac5a'
+const apiKey = '92b9cff50c9745fc9fc4212793f6ac5a'
 const baseCurrencySelect = document.querySelector('#baseCurrency')
 const targetCurrencySelect = document.querySelector('#targetCurrency')
-
-// console.log(baseCurrencySelect)
-
+const targetCurrencyInput = document.querySelector('target')
 
 
-function defaultExchange(){
-    let defaultBase = 'USD'
-    let defaultTarget = 
-    fetch(`https://exchange-rates.abstractapi.com/v1/live/?api_key=${apiUrl}&base=USD&target=EUR`)
+// Function for Loading default currency converter values 
+function getExchange(data){
+    const exchange = data.exchange_rates
+    console.log(exchange[0])
+    
+}
+
+// initializing function
+function initialize(){
+    baseCurrencySelect.selectedIndex = 0;
+    targetCurrencySelect.selectedIndex = 1;
+    const baseCurrency = baseCurrencySelect.value
+    const targetCurrency = targetCurrencySelect.value
+
+
+    fetch(`https://exchange-rates.abstractapi.com/v1/live/?api_key=${apiKey}&base=${baseCurrency}&target=${targetCurrency}`)
+    .then(response => response.json())
+    .then(data => {
+        getExchange(data)
+    })
+    .catch(error => {
+        console.error('Error occured:', error)
+    })
 }
 
 
 
-
 document.addEventListener('DOMContentLoaded', ()=> {
-
+    initialize()
 })
