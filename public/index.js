5428a1958c96032d3a57b664bbf6e9f008b37fcd
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-11-01 11:45:22 +0300

    Declared variables in index.js

---
+++ public/index.js
@@ -0,0 +1,20 @@
+const ApiKey = '92b9cff50c9745fc9fc4212793f6ac5a'
+const baseCurrencySelect = document.querySelector('#baseCurrency')
+const targetCurrencySelect = document.querySelector('#targetCurrency')
+
+// console.log(baseCurrencySelect)
+
+
+
+function defaultExchange(){
+    let defaultBase = 'USD'
+    let defaultTarget = 
+    fetch(`https://exchange-rates.abstractapi.com/v1/live/?api_key=${apiUrl}&base=USD&target=EUR`)
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', ()=> {
+
+})
