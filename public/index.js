5d433b2841ad96db929ca4a2323824a120a7bad1
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-11-05 19:45:14 +0300

    Added function for filling the select items with countries data

--- public/index.js
+++ public/index.js
@@ -101,13 +101,70 @@ const navLinks = document.querySelector('.nav').children
 for (const navLink of navLinks){
     navLink.addEventListener('click', (event)=>{
         const sectionId = navLink.id
-        console.log(sectionId)
+        // console.log(sectionId)
         
         if (sectionId){
             document.querySelector(`${sectionId}`).scrollIntoView({behavior: 'smooth'})
         }
     })
 }
+
+// Reference Select Elements
+const selectElements = document.querySelectorAll('select')
+// console.log(selectElements)
+
+function addCurrencyOptions(){
+    selectElements.forEach(select =>{
+        select.innerHTML = `
+        <option value="USD">US Dollar</option>
+        <option value="EUR">Euro</option>
+        <option value="ARS">Argentine Peso</option>
+        <option value="AUD">Australian Dollar</option>
+        <option value="BGN">Bulgarian Lev</option>
+        <option value="BCH">Bitcoin Cash</option>
+        <option value="BNB">Binance Coin</option>
+        <option value="BRL">Brazilian Real</option>
+        <option value="BTC">Bitcoin</option>
+        <option value="CAD">Canadian Dollar</option>
+        <option value="CHF">Swiss Franc</option>
+        <option value="CNY">Chinese Yuan</option>
+        <option value="CZK">Czech Republic Koruna</option>
+        <option value="DKK">Danish Krone</option>
+        <option value="DOGE">Dogecoin</option>
+        <option value="DZD">Algerian Dinar</option>
+        <option value="ETH">Ethereum</option>
+        <option value="GBP">British Pound Sterling</option>
+        <option value="HKD">Hong Kong Dollar</option>
+        <option value="HRK">Kroatian Kuna</option>
+        <option value="HUF">Hungarian Forint</option>
+        <option value="IDR">Indonesian Rupian</option>
+        <option value="ILS">Israeli New Sheqel</option>
+        <option value="INR">Indian Rupee</option>
+        <option value="ISK">Icelandic Krona</option>
+        <option value="JPY">Japanese Yen</option>
+        <option value="KRW">South korean Won</option>
+        <option value="LTC">Litecoin</option>
+        <option value="MAD">Moroccan Dirham</option>
+        <option value="MXN">Mexican Peso</option>
+        <option value="MYR">Malaysian Ringgit</option>
+        <option value="NOK">Norwegian Krone</option>
+        <option value="NZD">New Zealand Dollar</option>
+        <option value="PHP">Philippine Peso</option>
+        <option value="PLN">Polish Zloty</option>
+        <option value="RON">Romanian Leu</option>
+        <option value="RUB">Russian Ruble</option>
+        <option value="SEK">Swedish Krona</option>
+        <option value="SGD">Singapore Dollar</option>
+        <option value="THB">Thai Baht</option>
+        <option value="TRY">Turkish Lira</option>
+        <option value="TWD">New Taiwan Dollar</option>
+        <option value="XRP">Ripple</option>
+        <option value="ZAR">South African Rand</option> 
+        `
+    })
+}
+
+addCurrencyOptions();
 
 document.addEventListener('DOMContentLoaded', ()=> {
     
